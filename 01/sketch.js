4c3d8227941ed2de59e4a4ea3b905d3ab7e05d67
gisell-9 <42419707+gisell-9@users.noreply.github.com>
2018-08-15 14:33:25 -0500

    Add files via upload

---
+++ 01/sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(216,210,201);
+stroke(118,116,111);
+fill(118,116,111);
+ellipse(300,375,300,300);
+stroke(183,152,84);
+fill(183,152,84,100);
+triangle(290,0,75,231,370,320);
+stroke(12,12,12);
+fill(110,30,25);
+rect(422,460,130,130);
+stroke(12,12,12);
+strokeWeight(6);
+line(330,0,600,440);
+strokeWeight(10);
+stroke(12,12,12);
+line(0,60,290,360);
+strokeWeight(5);
+stroke(12,12,12);
+line(0,202,190,202);
+stroke(12,12,12);
+strokeWeight(2);
+line(0,460,280,460);
+stroke(12,12,12);
+strokeWeight(4);
+line(490,520,600,520);
+stroke(12,12,12);
+strokeWeight(4);
+line(490,540,600,540);
+strokeWeight(4);
+
+}
